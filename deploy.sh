9e702ed80730127243909f364b140bc18d98d672
meta-harsh <harsh@metadrob.com>
2025-02-07 07:46:22 +0530

    Deploy: using vite docs again

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:harshkhasbage77/startup-website.git main:gh-pages
 
 cd -
