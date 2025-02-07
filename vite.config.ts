f7bebdf441d77c05795b7b29e930b2fcae67dd1f
meta-harsh <harsh@metadrob.com>
2025-02-07 07:44:56 +0530

    Deploy: using vite docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://harshkhasbage77.github.io/startup-website/',
   plugins: [react()],
 })
